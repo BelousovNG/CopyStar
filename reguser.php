f57378b6a519b947a7b9362668313c034675b988
BelousovNG <99665330+BelousovNG@users.noreply.github.com>
2022-04-15 13:57:19 +0300

    Add files via upload

--- reguser.php
+++ reguser.php
@@ -1,62 +1,18 @@
-<?php require_once "blocks/header.php";?>
-<main>
-    <div class="container">
-        <div class="reg_user">
-            <form action="php/reguser.php" method="post" id="reg_form">
-                <h1>Регистрация</h1>
-                <br>
-                <h5 class="sub__title">Личный данные</h5>
-                <div class="cols">
-                    <p>
-                        <label for="">Имя</label>
-                        <input type="text" placeholder="" name="name" required>
-                    </p>
-                    <p>
-                        <label for="">Фамилия</label>
-                        <input type="text" placeholder="" name="surname" required>
-                    </p>
-                    <p>
-                        <label for="">Отчество</label>
-                        <input type="text" placeholder="" name="secondname" required>
-                    </p>
-                    <!-- <p>
-                        <label for="">Пол</label>
-                        <select name="gender" id="gender" required>
-                            <option value="man">Мужчина</option>
-                            <option value="woman">Женщина</option>
-                        </select>
-                    </p> -->
+<?php
+$user=$_POST['user'];
+include "connect.php";
+session_start();
+$sql="SELECT * FROM `users` WHERE `login`='$user[login]'";
+$check=$mysql->query($sql);
 
-                </div>
+if($check->num_rows>=1){
+    echo 'logerror';
+    return;
+}
 
-                <h5 class="sub__title">Данные для идентификации</h5>
-                <div class="cols">
-                    <p>
-                        <label for="">Логин</label>
-                        <input type="text" placeholder="" name="login" id="login_res">
-                    </p>
-                    <p>
-                        <label for="">Email</label>
-                        <input type="email" placeholder="" name="mail" >
-                    </p>
-                    <p>
-                        <label for="">Пароль</label>
-                        <input type="password" placeholder="" name="pass_reg" id="pass_reg">
-                    </p>
-                    <p>
-                        <label for="">Повторите пароль</label>
-                        <input type="password" placeholder="" name="pass_rep" id="pass_rep">
-                    </p>
-                    <p>
-                        <input type="checkbox" placeholder="" name="" required>
-                        <label for="">Согласение на обработку</label>
-                    </p>
-                    <p class="errormsg"></p>
-                </div>
-                <input type="submit" value="Зарегистрироваться" class="subbtn" >
-            </form>
+$sql="INSERT INTO `users` (`name_user`, `surname_user`, `secondname_user`, `login`, `pass`, `role`) VALUES ('$user[name]', '$user[surname]', '$user[secondname]', '$user[login]', '$user[pass_reg]', 'user')";
+$mysql->query($sql);
+$sql="SELECT * FROM `users` WHERE `login`='$user[login]'";
+$user=$mysql->query($sql);
 
-        </div>
-    </div>
-</main>
-<?php require_once "blocks/footer.php";?>
+$_SESSION['user']=$user->fetch_assoc();
